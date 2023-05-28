7fcd200f3e1304ee8973ef3d4d50573b43e2c6a3
Hoseok Seong <shsshs0125@naver.com>
2023-05-28 20:05:52 +0900

    배포 1.6

--- src/main/java/shop/donutmarket/donut/domain/main/controller/MainController.java
+++ src/main/java/shop/donutmarket/donut/domain/main/controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
